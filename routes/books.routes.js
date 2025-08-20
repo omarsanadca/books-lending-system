4cf39ce84ae8429b97697b1cad907b9c81e80811
omarsanadca <omarsanadca@gmail.com>
2025-08-20 20:55:27 +0300

    mysql done

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -4,51 +4,55 @@ import express from "express";
 import { clearAndWrite, read, write } from "../utils/file-utils.js";
 import { fixBody } from "../utils/fixBody.js";
 
+import db from "../utils/db.js";
+
 const booksKeys = ["title", "authorId", "year", "isBorrowed"];
 
 const router = express.Router();
 
 router.get("", async (req, res) => {
   try {
-    let books = await read("books");
+    // let books = await read("books");
 
-    const authors = await read("authors");
+    // const authors = await read("authors");
 
-    books = books.map((b) => {
-      const newBook = { ...b };
-      const authorId = newBook.authorId;
-      delete newBook.authorId;
+    // books = books.map((b) => {
+    //   const newBook = { ...b };
+    //   const authorId = newBook.authorId;
+    //   delete newBook.authorId;
 
-      const authorData = authors.find((a) => a.id === authorId);
+    //   const authorData = authors.find((a) => a.id === authorId);
 
-      newBook.author = authorData;
+    //   newBook.author = authorData;
 
-      return newBook;
-    });
+    //   return newBook;
+    // });
 
-    const { sort, borrowed } = req.query;
+    // const { sort, borrowed } = req.query;
 
-    if (sort) {
-      if (sort === "asc") {
-        books.sort((a, b) => {
-          if (a.title < b.title) return -1;
-          return 1;
-        });
-      } else {
-        books.sort((a, b) => {
-          if (a.title > b.title) return -1;
-          return 1;
-        });
-      }
-    }
+    // if (sort) {
+    //   if (sort === "asc") {
+    //     books.sort((a, b) => {
+    //       if (a.title < b.title) return -1;
+    //       return 1;
+    //     });
+    //   } else {
+    //     books.sort((a, b) => {
+    //       if (a.title > b.title) return -1;
+    //       return 1;
+    //     });
+    //   }
+    // }
 
-    if (borrowed !== undefined) {
-      if (borrowed === "true") {
-        books = books.filter((b) => b.isBorrowed);
-      } else {
-        books = books.filter((b) => !b.isBorrowed);
-      }
-    }
+    // if (borrowed !== undefined) {
+    //   if (borrowed === "true") {
+    //     books = books.filter((b) => b.isBorrowed);
+    //   } else {
+    //     books = books.filter((b) => !b.isBorrowed);
+    //   }
+    // }
+
+    const [books] = await db.execute("SELECT * FROM books;");
 
     res.json({ message: "books fetched successfully", books });
   } catch (err) {
@@ -63,15 +67,23 @@ router.get("/:id", async (req, res) => {
   try {
     const { id: bookId } = req.params;
 
-    const books = await read("books");
+    // const books = await read("books");
 
-    const book = books.find((b) => b.id === bookId);
+    // const book = books.find((b) => b.id === bookId);
 
-    if (!book) {
-      return res.status(404).json({ message: "Book Not Found" });
+    // if (!book) {
+    //   return res.status(404).json({ message: "Book Not Found" });
+    // }
+
+    const [books] = await db.execute("SELECT * FROM books where id = ?;", [
+      bookId,
+    ]);
+
+    if (books.length === 0) {
+      return res.status(404).json({ message: "Book not found!" });
     }
 
-    res.json({ message: "Fetched book", book });
+    res.json({ message: "Fetched book", book: books[0] });
   } catch (err) {
     console.log("Error when GET /books/:id -> ", err.message);
     res
@@ -90,21 +102,27 @@ router.post("", async (req, res) => {
       return res.status(400).json({ message: "invalid arguments" });
     }
 
-    const authors = await read("authors");
+    // const authors = await read("authors");
 
-    const authorIndex = authors.findIndex((a) => a.id === authorId);
+    // const authorIndex = authors.findIndex((a) => a.id === authorId);
 
-    if (authorIndex === -1) {
-      return res.status(404).json({ message: "author not found" });
-    }
+    // if (authorIndex === -1) {
+    //   return res.status(404).json({ message: "author not found" });
+    // }
 
-    const newBook = { id: nanoid(), ...booksData };
+    // const newBook = { id: nanoid(), ...booksData };
 
-    await write("books", newBook);
+    // await write("books", newBook);
 
-    res
-      .status(201)
-      .json({ message: "book created successfully", book: newBook });
+    const [result] = await db.execute(
+      "INSERT INTO books (title, year) VALUES (?, ?)",
+      [title, year]
+    );
+
+    res.status(201).json({
+      message: "book created successfully",
+      book: { id: result.insertId, title, year },
+    });
   } catch (err) {
     console.log("Error when POST /books/ -> ", err.message);
     res
@@ -118,19 +136,26 @@ router.patch("/:id", async (req, res) => {
     const { id: bookId } = req.params;
     const booksData = fixBody(req.body, booksKeys);
 
-    const books = await read("books");
+    // const books = await read("books");
 
-    const bookIndex = books.findIndex((b) => b.id === bookId);
+    // const bookIndex = books.findIndex((b) => b.id === bookId);
 
-    if (bookIndex === -1) {
-      return res.status(404).json({ message: "Book Not Found" });
-    }
+    // if (bookIndex === -1) {
+    //   return res.status(404).json({ message: "Book Not Found" });
+    // }
 
-    books[bookIndex] = { ...books[bookIndex], ...booksData };
+    // books[bookIndex] = { ...books[bookIndex], ...booksData };
 
-    await clearAndWrite("books", books);
+    // await clearAndWrite("books", books);
 
-    res.json({ message: "book updated!", updatedBook: books[bookIndex] });
+    const [result] = await db.execute(
+      "UPDATE books SET year = ? WHERE id = ?",
+      [booksData.year, bookId]
+    );
+
+    console.log(result);
+
+    res.json({ message: "book updated!", updatedBook: {} });
   } catch (err) {
     console.log("Error when PATCH /books/:id -> ", err.message);
     res
@@ -143,19 +168,40 @@ router.delete("/:id", async (req, res) => {
   try {
     const { id: bookId } = req.params;
 
-    const books = await read("books");
+    // const books = await read("books");
 
-    const bookIndex = books.findIndex((b) => b.id === bookId);
+    // const bookIndex = books.findIndex((b) => b.id === bookId);
 
-    if (bookIndex === -1) {
-      return res.status(404).json({ message: "Book Not Found" });
+    // if (bookIndex === -1) {
+    //   return res.status(404).json({ message: "Book Not Found" });
+    // }
+
+    // books.splice(bookIndex, 1);
+
+    // clearAndWrite("books", books);
+
+    const [result] = await db.execute("DELETE FROM books WHERE id = ?", [
+      bookId,
+    ]);
+
+    if (result.affectedRows) {
+      res.json({ message: "book deleted successfully" });
+    } else {
+      res.status(404).json({ message: "book not found!" });
     }
+  } catch (err) {
+    console.log("Error when DELETE /books/:id -> ", err.message);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error, please try again later." });
+  }
+});
 
-    books.splice(bookIndex, 1);
+router.delete("/", async (req, res) => {
+  try {
+    const [result] = await db.execute("DELETE FROM books");
 
-    clearAndWrite("books", books);
-
-    res.json({ message: "book deleted successfully" });
+    res.json({ message: "all books were deleted" });
   } catch (err) {
     console.log("Error when DELETE /books/:id -> ", err.message);
     res
